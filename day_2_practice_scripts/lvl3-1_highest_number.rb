8680c91a1dddc241a6fe817afdeecd4d5500ef1b
asilvsetre1106 <asilvestre@gbox.adnu.edu.ph>
2026-06-11 14:01:04 +0800

    find the largest number

---
+++ day_2_practice_scripts/lvl3-1_highest_number.rb
@@ -0,0 +1,21 @@
+print "Please enter 5 numbers\n"
+
+numbers = Array.new
+
+for i in 1..5 
+  print "Enter number #{i}: "
+  num = gets.chomp.to_i
+  numbers.push(num)
+end
+
+p numbers
+
+largest = numbers[0]
+
+for i in 0..4
+  if numbers[i] >= largest
+    largest = numbers[i]
+  end
+end
+
+puts "Largest number: #{largest}"
